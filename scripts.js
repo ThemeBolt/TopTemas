440ac2a60c5c7f4e3f296d554f586eb0b8273dda
ThemeBolt <xavier@lojadelayouts.com>
2025-05-07 16:14:43 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.querySelector('.menu-toggle');
-    const menu = document.getElementById('menu');
-
-    toggleButton.addEventListener('click', () => {
-      menu.classList.toggle('show');
-      toggleButton.setAttribute('aria-expanded', menu.classList.contains('show'));
-    });
-
-    
